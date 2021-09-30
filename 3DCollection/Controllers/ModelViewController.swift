55cb5cdc18a1047d145670c835d26dc298b4495e
João Pedro Picolo <picolojoaop@gmail.com>
2021-09-30 10:05:15 -0300

    Initial commit

---
+++ 3DCollection/Controllers/ModelViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ModelViewController.swift
+//  3DCollection
+//
+//  Created by João Pedro Picolo on 30/09/21.
+//
+
+import UIKit
+
+class ModelViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        title = "Model"
+    }
+}
+
---
+++ 3DCollection/ViewController.swift
@@ -0,0 +1,14 @@
+//
+//  ViewController.swift
+//  3DCollection
+//
+//  Created by João Pedro Picolo on 30/09/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
