587cb2327f6f4aa59715bd2a7950f189f20ecf72
João Pedro Picolo <picolojoaop@gmail.com>
2021-10-01 09:38:44 -0300

    Refactors ARView position to show in same page as model

--- 3DCollection/Controllers/ModelViewController.swift
+++ 3DCollection/Controllers/ModelViewController.swift
@@ -6,10 +6,12 @@
 //
 
 import UIKit
+import ARKit
+import RealityKit
 
 class ModelViewController: UIViewController {
-    private let width = UIScreen.main.bounds.size.width
-    private let height = UIScreen.main.bounds.size.height
+    @IBOutlet var arView: ARView!
+    @IBOutlet var contentView: UIView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,49 +20,82 @@ class ModelViewController: UIViewController {
     
     private func setupLayout() {
         view.insetsLayoutMarginsFromSafeArea = false
-        
-        placeModelImage()
-        placeModelInformation()
+
+        setUpContent()
     }
     
-    private func placeModelImage() {
-        let imageView = UIImageView(frame: CGRect(x: 0, y: 0, width: width, height: height / 3.5))
-        imageView.image = UIImage(named: "saturn")
-        imageView.layer.cornerRadius = imageView.frame.height / 5.5
-        imageView.layer.masksToBounds = true
-        
-        view.addSubview(imageView)
+    private func setUpContent() {
+        placeModelInformation(width: contentView.frame.width)
+        placeModelDescription(width: contentView.frame.width)
     }
     
-    private func placeModelInformation() {
-        let screenHalfWidth = width / 2
-        let initialY = height / 3.5
-        let verticalPadding: CGFloat = 25
-        let horizontalPadding: CGFloat = 20
+    private func placeModelInformation(width: CGFloat) {
+        let viewHalfWidth = width / 2
         
-        let informationView = UIView(frame: CGRect(x: 0, y: initialY + verticalPadding, width: width, height: 60))
-        
-        let nameView = UITextView(frame: CGRect(x: horizontalPadding, y: 0, width: screenHalfWidth, height: informationView.frame.height))
+        let nameView = UITextView(frame: CGRect(x: 0, y: 0,
+                                                width: viewHalfWidth, height: 50))
         nameView.text = "Saturn"
         nameView.font = .systemFont(ofSize: 34, weight: .bold)
+        contentView.addSubview(nameView)
         
-        let button = UIButton(frame: CGRect(x: screenHalfWidth, y: 0, width: screenHalfWidth, height: informationView.frame.height))
-        button.setTitle("Open Model >", for: .normal)
-        button.setTitleColor(.black, for: .normal)
-        button.addTarget(self, action: #selector(openARView), for: .touchUpInside)
-        
-        informationView.addSubview(nameView)
-        informationView.addSubview(button)
-        view.addSubview(informationView)
+
+        let button = UIButton(frame: CGRect(x: viewHalfWidth, y: 0,
+                                            width: viewHalfWidth, height: 60))
+        button.setTitle("Settings >", for: .normal)
+        button.setTitleColor(nameView.textColor, for: .normal)
+        button.addTarget(self, action: #selector(openSettings), for: .touchUpInside)
+        contentView.addSubview(button)
+    }
+    
+    private func placeModelDescription(width: CGFloat) {
+        let label = UILabel(frame: CGRect(x: 0, y: 80,
+                                          width: width, height: 20))
+        label.text = "Project Description"
+        label.font = .systemFont(ofSize: 17, weight: .bold)
+        contentView.addSubview(label)
+
+        let description = UILabel(frame: CGRect(x: 0, y: 80,
+                                                width: width - 15, height: 120))
+        description.text = "That's a very cool project found on NASA's website and used as a model for my Apps's ideia at the Apple Developer Academy."
+        description.numberOfLines = 0
+        description.font = .systemFont(ofSize: 14, weight: .regular)
+        contentView.addSubview(description)
     }
     
     @objc
-    func openARView() {
-        let storyBoard: UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
-        let arViewController = storyBoard.instantiateViewController(withIdentifier: "ARViewController") as! ARViewController
-        arViewController.modalPresentationStyle = .fullScreen
-        arViewController.modalTransitionStyle = .flipHorizontal
-        self.present(arViewController, animated: true, completion: nil)
+    func openSettings() {
+        print("Settings here")
     }
 }
 
+
+extension ModelViewController {
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        setupARView()
+        setupEntities()
+    }
+      
+    // MARK: Setup Methods
+    func setupARView() {
+        arView.cameraMode = .ar
+        arView.automaticallyConfigureSession = true
+    }
+    
+    func setupEntities() {
+        let modelEntity = try! ModelEntity.loadModel(named: "Saturn")
+        modelEntity.generateCollisionShapes(recursive: true)
+        enableGestures(modelEntity)
+
+        let anchorEntity = AnchorEntity(plane: .horizontal)
+        anchorEntity.scale = SIMD3(SCNVector3(x: 0.2, y: 0.2, z: 0.2))
+        anchorEntity.addChild(modelEntity)
+        
+        arView.scene.addAnchor(anchorEntity)
+    }
+    
+    func enableGestures(_ modelEntity: ModelEntity) {
+        arView.installGestures([.translation, .rotation, .scale], for: modelEntity)
+    }
+}
